1d4944f9350389de656b3e60226543cd0d6b57c9
hamzaFathi <125149161+hamzaFathi@users.noreply.github.com>
2024-01-22 19:17:33 +0200

    login.NET

---
+++ Login/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Login.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Login.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Login(Log enter)
+        {
+            if (enter == null)
+            {
+                return RedirectToAction("Index");
+            }
+            
+
+            return View();
+        }
+
+
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Login/Models/Log.cs
@@ -0,0 +1,8 @@
+﻿namespace Login.Models
+{
+    public class Log
+    {
+        public string Name { get; set; }=string.Empty; 
+        public int Pass { get; set;}
+    }
+}
